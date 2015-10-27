dd9fbc080d918e6351e00b94dc476d5ad78b8a98
appli456 <lirunzhi99@hotmail.com>
2015-10-27 19:10:44 +0800

    元素拖动完成，添加轮播js文件，更换了拖动图片

--- js/Girl.move.js
+++ js/Girl.move.js
@@ -1,52 +1,103 @@
 /**
  * Created by li_rz on 2015/9/6.
  */
-/*girl.move =*/ (function () {
+girl.move = (function () {
     'use strict';
     // ------------------------ 变量定义与声明 -----------------------------
 
 
-    var drag,
-        offset = {
-            x : null,
-            y : null
-        },
-        doll,
-        touch = document.ontouchstart !== undefined,
-        updatePosition;
-
-
+    var drag = false,
+        moveDoll,
+        initModule;
 
     // ---------------------- 结束变量定义与声明 -----------------------------
 
     // ---------------------- DOM 方法 -------------------------------
 
-    updatePosition = function (event) {
-        //var wrapper = {
-        //    x :  doll.offsetLeft,
-        //    y :  doll.offsetTop
-        //};
+    moveDoll = function(doll){
 
-        var targetPosition = {
-            x : null,
-            y : null
-        };
+        // 点击时鼠标坐标
+        var img = doll.find('img'),
+            clickDownPosition = {
+                x : null,
+                y : null
+            },
+            imgSize = {
+                x :  null,
+                y : null
+            },
+            distanceFromClickAndOffset = {
+                x : null,
+                y : null
+            };
 
-        if (touch) {
-            event.preventDefault();
-        }
+        console.log(img);
+        imgSize.x = parseInt(img.css('width'));
+        imgSize.y = parseInt(img.css('height'));
 
-        console.log(event.pageX);
-        console.log(offset.x);
-        targetPosition.y = (event.pageY- window.scrollY)/*.toString() + 'px'*/;
-        targetPosition.x = (event.pageX)/*.toString() + 'px'*/;
-        console.log(targetPosition);
+        console.log(imgSize);
+        doll.on('mousedown',  function(event){
+            drag = true;
+            var $this = this,
 
-        // 有问题
-        doll.css('top', event.pageY);
-        doll.css('left', event.pageX);
-        //
-        console.log(doll);
+            // 元素位置
+                offset = {
+                    x : $this.offsetLeft,
+                    y : $this.offsetTop
+                };
+
+            clickDownPosition.x = event.clientX;
+            clickDownPosition.y = event.clientY;
+
+            distanceFromClickAndOffset = {
+                x : clickDownPosition.x - offset.x,
+                y : clickDownPosition.y - offset.y
+            };
+
+            doll.on('mousemove', function(event){
+                console.log('offset:',  offset.x, offset.y);
+                console.log('Distance:', distanceFromClickAndOffset.x, distanceFromClickAndOffset.y);
+                console.log('ImgSize:', imgSize.x, imgSize.y);
+                console.log('client:', event.clientX, event.clientY);
+                if(!drag) {
+                    return false;
+                }
+                var enterLeft = event.clientX - distanceFromClickAndOffset.x < 0,
+                    enterTop = event.clientY - distanceFromClickAndOffset.y < 0,
+                    enterRight = event.clientX > screen.availWidth - imgSize.x + distanceFromClickAndOffset.x,
+                    enterBottom = event.clientY > screen.availHeight - imgSize.y + distanceFromClickAndOffset.y;
+
+                if (enterLeft || enterTop || enterRight || enterBottom) {
+                    if (enterLeft) {
+                        $this.style.left = 0 + 'px';
+                    } else if (enterRight) {
+                        $this.style.left = screen.availWidth - imgSize.x + "px";
+                    } else {
+                        $this.style.left = offset.x + event.clientX - clickDownPosition.x + "px";
+                    }
+
+                    if (enterTop) {
+                        $this.style.top = 0 + 'px';
+                    } else if (enterBottom) {
+                        $this.style.top = screen.availHeight - imgSize.y + "px";
+                    } else {
+                        $this.style.top = offset.y + event.clientY - clickDownPosition.y + "px";
+                    }
+                } else {
+                    $this.style.left = offset.x + event.clientX - clickDownPosition.x + "px";
+                    $this.style.top = offset.y + event.clientY - clickDownPosition.y + "px";
+                }
+
+            });
+
+            doll.on('mouseup', function() {
+                drag = false;
+            });
+
+            doll.on('mouseleave', function () {
+                drag = false;
+            });
+        });
     };
 
 
@@ -56,29 +107,19 @@
 
 
 
-    document.addEventListener('DOMContentLoaded', function () {
-        doll = $('#Doll');
 
-        doll.on(touch ? 'touchstart' : 'mousedown', function (event) {
-            drag = event.target;
-
-            if (drag) {
-                offset = {
-                    x : event.offsetX || event.layerX,
-                    y : event.offsetY || event.layerY
-                };
-
-                doll.on(touch ? 'touchstart' : 'mousedown', updatePosition);
-            }
-        });
-    });
 
     // --------------------- 结束事件 ----------------------------
 
     // --------------------- 公共方法 ----------------------------
 
 
+    initModule = function (doll) {
+        doll.on('mouseover', moveDoll(doll));
+    };
     // -------------------- 结束公共方法 --------------------------
 
-
+    return {
+        initModule: initModule
+    }
 }());
